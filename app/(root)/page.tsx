1549b361c03a8ffb1a63da4e4b8fa6e26718ad13
Adedamola Maxwell <promcode01@gmail.com>
2025-12-15 16:47:00 +0100

    Update app/(root)/page.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 const Page = () => {
   return (
-    <div className="flex min-h-screen home-wrapper boder border-red-60">
+    <div className="flex min-h-screen home-wrapper border border-red-600">
       <Header />
       <div className="container mx-auto">
         <Button>Button</Button>
